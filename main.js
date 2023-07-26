5eabc727c94145e509f171ae8cbffec96c3cfbb1
sofibear01 <sofibearzotti01@gmail.com>
2023-07-25 21:46:20 -0300

    agrego cambios de clase 20

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu); //toggle seria como intercambiar, para indicar que se muestra y se esconde tambien
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive'); //esto quita o pone la clase inactive 
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
